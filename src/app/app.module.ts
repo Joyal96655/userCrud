5c591109462e5ec17699a036ce5ef7f3b20b96d0
Joyal Abraham <joyal.abraham@cloudesign.in>
2020-11-13 17:56:35 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './component/user/user.component';
+import { HomeComponent } from './component/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/interceptor.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+// tslint:disable-next-line:quotemark
+import { HttpInterceptor, HttpHandler, HttpRequest, HttpEvent } from "@angular/common/http";
+import { Observable } from 'rxjs';
+
+import { environment } from '../environments/environment';
+export class Interceptor implements HttpInterceptor {
+    intercept(
+        req: HttpRequest<any>,
+        next: HttpHandler
+      ): Observable<HttpEvent<any>> {
+        const loadingContainer: HTMLElement = document
+          .getElementsByClassName('popup')
+          .item(0) as HTMLElement;
+
+        /* sanket start */
+        const loadingContainerForCustomError: HTMLElement = document
+          .getElementsByClassName('customPopup')
+          .item(0) as HTMLElement;
+        const myErrorMsg: HTMLElement = document
+          .getElementsByClassName('myErrorMsg')
+          .item(0) as HTMLElement;
+
+        const url = environment.apiUrl;
+
+        req = req.clone({
+            url: url + req.url,
+            setHeaders: {
+              // "Content-Type": "application/json",
+              Accept: 'application/json',
+              AuthToken: 'ePnnWv6zrcldla62vUP5XhFw3W89kI3N',
+            },
+          });
+        console.log('req', req);
+        return next.handle(req);
+      }
+  }
